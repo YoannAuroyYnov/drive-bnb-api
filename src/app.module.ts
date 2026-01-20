63ae95135785fd4bb5f5c67c8c875c7ed11e7d8d
Yoann Auroy <yoann.ar.pro@gmail.com>
2026-01-20 12:01:46 +0100

    init users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VehiclesModule } from './vehicles/vehicles.module';
 import { BookingsModule } from './bookings/bookings.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [VehiclesModule, BookingsModule],
+  imports: [VehiclesModule, BookingsModule, UsersModule],
   controllers: [],
   providers: [],
 })
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  lastName?: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  is2FAEnabled?: boolean;
+}
