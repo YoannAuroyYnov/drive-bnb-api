68f7de459941996c229890c20347a330b46a33df
Yoann Auroy <yoann.ar.pro@gmail.com>
2026-01-23 14:58:16 +0100

    enable cors, add public routes

--- src/vehicles/vehicles.controller.ts
+++ src/vehicles/vehicles.controller.ts
@@ -3,15 +3,28 @@ import { VehiclesService } from './vehicles.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { VehiclesFilterParamsDto } from './dto/vehicle-filter-params.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('vehicles')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class VehiclesController {
   constructor(private readonly vehiclesService: VehiclesService) {}
 
+  @Public()
   @Get()
-  findAll(@Query() query: VehiclesFilterParamsDto) {
+  findAll() {
+    return this.vehiclesService.findAll();
+  }
+
+  @Get('filtered')
+  findFiltered(@Query() query: VehiclesFilterParamsDto) {
     return this.vehiclesService.findAll(query);
+  }
+
+  @Public()
+  @Get('featured')
+  findFeatured() {
+    return this.vehiclesService.findFeatured();
   }
 
   @Get(':id')
--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm/repository/Repository';
+import { Repository, In } from 'typeorm';
 import { Vehicle } from './entities/vehicle.entity';
 import { VehiclesFilterParamsDto } from './dto/vehicle-filter-params.dto';
 
@@ -11,18 +11,34 @@ export class VehiclesService {
     private vehicleRepository: Repository<Vehicle>,
   ) {}
 
-  async findAll(query: VehiclesFilterParamsDto) {
-    const whereConditions = { isAvailable: query.isAvailable };
-    if (query.type) {
+  async findAll(query?: VehiclesFilterParamsDto) {
+    const whereConditions = {};
+    if (query) {
       whereConditions['vehicleType'] = { name: query.type };
+      // Add date range filtering logic here if applicable
     }
     return await this.vehicleRepository.find({
       where: whereConditions,
-      order: { updatedAt: query.order },
+      order: { updatedAt: query?.order || 'DESC' },
     });
   }
 
   async findOne(id: string) {
     return await this.vehicleRepository.findOneBy({ id });
+  }
+
+  async findFeatured() {
+    const ids = [
+      '6a893432-d23a-454f-add4-ec3df8b2f01e',
+      '03c51c22-a2fb-45b8-b91a-c2fa96c6bb39',
+      'd5712f88-c192-4bce-aae7-0fbb42425e98',
+    ];
+    const result = await this.vehicleRepository.find({
+      where: { id: In(ids) },
+      order: { updatedAt: 'DESC' },
+    });
+
+    console.log('Featured vehicles fetched:', result);
+    return result;
   }
 }
